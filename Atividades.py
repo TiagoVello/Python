8149dc688ba59fc2db6d73321b72c23441933b3c
TiagoVello <39015272+TiagoVello@users.noreply.github.com>
2019-05-23 13:45:54 -0300

    13.45

--- Atividades.py
+++ Atividades.py
@@ -1,7 +1,56 @@
 import pandas as pd
 
-class ListadeAtividade ():
-    def __init__(self, file):
-        data = pd.read_excel(file)
+nomes = ['Pinduca',
+         'Prec',
+         'Ed',
+         'Mosko',
+         'Foquinha',
+         'Kuri',
+         'Shao',
+         'Palestrinha',
+         'Migo',
+         'Mingau',
+         'Dorinha',
+         'Buzz',
+         'Cidão',
+         'Dolfinho',
+         'Shaozinho',
+         'Didi']
+         
+atividades = [['Colocar ração para os cachorros e para as gatas','4x por dia para as gatas e 2x por dia para os cachorros'],
+              ['Colocar ração para os cachorros e para as gatas','4x por dia para as gatas e 2x por dia para os cachorros'],
+              ['Fazer gelo e deixar a garrafa de agua cheia','diária'],
+              ['Lixos DO BANHEIRO COMUM E DA COZINHA (repor saco de lixo)','diária'],
+              ['Passear com os dogs','2x por semana'],
+              ['Passear com os dogs','2x por semana'],
+              ['Varrer area comum','2x por semana'],
+              ['Varrer area comum','2x por semana'],
+              ['Lavar lixos','Semanal'],
+              ['Assuntos Pontuais','---------'],
+              ['Recolher louça suja segunda feira','1x por semana'],
+              ['Guardar louça limpa e seca','diária'],
+              ['Limpar a areia','2x por dia'],
+              ['Pegar as merda dos dogs','2x por semana'],
+              ['Passar a redinha','diária'],
+              ['Limpar a geladeira. Tirar panelas e coisas estragadas','semanal']]
+    
+def exibir(atividades, nomes):
+    print('LISTA DE ATIVIDADES')
+    print('')
+    for i in range(len(nomes)):
+        print('Parabéns, {} foi presenteado com: {} de frequência {}'.format(nomes[i],atividades[i][0],atividades[i][1]))
+        print('')
         
-        
+def proxima_atividade(atividades):
+    salvar_atividades = atividades[-1]
+    for i in range(len(nomes)-1):
+        atividades[-1-i] = atividades[-2-i]
+    atividades[0] = salvar_atividades
+    return atividades
+
+proxima_atividade(atividades)
+exibir(atividades,nomes)
+
+print(len(atividades))
+print(len(nomes))
+    
